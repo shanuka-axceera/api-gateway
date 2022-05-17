965c45f358ed1abe34610a57c51edf7b1cc81675
Kavishka Gardiarachchi <it19213590@my.sliit.lk>
2022-05-17 23:27:55 +0530

    add cart function.

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-const express = require('express');
-const gateway = require('fast-gateway');
-const port = 9001
+const express = require("express");
+const gateway = require("fast-gateway");
+const port = 9001;
 
-const server =gateway({
-    routes:[
-        {
-            prefix : '/abc',
-            target:'http://localhost:5010/',
-            hooks:{
-            }
-        }
-    ]
-})
+const server = gateway({
+  routes: [
+    {
+      prefix: "/abc",
+      target: "http://localhost:5010/",
+      hooks: {},
+    },
+    {
+      prefix: "/cart",
+      target: "http://localhost:5002/",
+      hooks: {},
+    },
+  ],
+});
 
-server.get('/my',(req,res)=>res.send("its working"));
+server.get("/my", (req, res) => res.send("its working"));
 
-server.start(port).then(server=>{
-    console.log("API gateway is running");
-})
+server.start(port).then((server) => {
+  console.log("API gateway is running");
+});
